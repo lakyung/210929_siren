b5c530e93bd0b73a1f3e827128d1b4cd2accd65a
lakyung <90668130+lakyung@users.noreply.github.com>
2021-09-28 11:40:10 +0900

    10번 반복하기 변경

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
-music.setVolume(12)
-// LED 핀 연결시 마이크로비트 기본 설정이 signal 1인 핀이 있음. ex) P3,P4..
-// 출력 가능 핀 : P1,P2,P5,P7...P13?
-// 
-// P0 : 소리 출력
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+    music.setVolume(0)
+})
+music.setVolume(169)
+for (let index = 0; index < 10; index++) {
     music.playTone(392, music.beat(BeatFraction.Whole))
     pins.digitalWritePin(DigitalPin.P1, 1)
     pins.digitalWritePin(DigitalPin.P2, 1)
@@ -16,4 +18,4 @@ basic.forever(function () {
     pins.digitalWritePin(DigitalPin.P5, 0)
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P13, 1)
-})
+}
